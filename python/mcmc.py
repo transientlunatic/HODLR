a0733359021da57ba02f6797c33389c603f3caf6
Dan F-M <danfm@nyu.edu>
2014-02-18 20:57:06 +0000

    square hyperparams

--- python/mcmc.py
+++ python/mcmc.py
@@ -42,7 +42,7 @@ def lnprior(lna, lns, fstar, q1, q2, t0, tau, ror, b):
 
 
 def lnlike(lna, lns, fstar, q1, q2, t0, tau, ror, b):
-    a2, s2 = np.exp(2*lna), np.exp(2*lns)
+    a, s = np.exp(lna), np.exp(lns)
 
     # Compute the model and residuals.
     res = f - model(fstar, q1, q2, t0, tau, ror, b)
@@ -52,9 +52,15 @@ def lnlike(lna, lns, fstar, q1, q2, t0, tau, ror, b):
     pl.savefig("res.png")
 
     # Solve the GP likelihood.
-    matrix = hodlr.HODLR(a2, s2, t, fe2)
+    matrix = hodlr.HODLR(a, s, t, fe2)
     alpha = matrix.solve(res)
     logdet = matrix.logdet()
+    print(logdet)
+
+    py_matrix = a*a*np.exp(-0.5 * (t[:, None] - t[None, :])**2 / (s*s))
+    py_matrix[(range(len(t)), range(len(t)))] += fe2
+    print(np.linalg.slogdet(py_matrix))
+
     if not np.isfinite(logdet):
         return -np.inf
     return -0.5 * (np.dot(res, alpha) + logdet + norm)
@@ -86,21 +92,23 @@ f, fe = f / np.median(fe), fe / np.median(fe)
 t -= np.min(t)
 
 # FIXME: just use a small amount of data.
-# m = (6 < t) * (t < 15)
-# t, f, fe, q = t[m], f[m], fe[m], q[m]
+m = (9 < t) * (t < 13)
+t, f, fe, q = t[m], f[m], fe[m], q[m]
 
 # Pre-compute the normalization factor for the log-likelihood.
 fe2 = fe * fe
 norm = len(t) * np.log(2 * np.pi)
 
 # Inject a transit.
-p0 = np.array([1e-10, 2.0, np.median(f), q1, q2, t0, tau, ror, b])
+p0 = np.array([-6.34475286e-08, 2.0, np.median(f), q1, q2, t0, tau, ror, b])
+# p0 = np.array([1e-10, 2.0, np.median(f), q1, q2, t0, tau, ror, b])
 f *= model(1, *(p0[3:]))
 
 # Compute the log likelihood.
 strt = time.time()
 print(lnprob(p0))
 print(time.time() - strt)
+assert 0
 
 # Plot the initial data.
 pl.clf()
@@ -108,10 +116,11 @@ pl.plot(t, f, ".k", alpha=0.3)
 pl.savefig("data.png")
 
 # Run MCMC.
-ndim, nwalkers = len(p0), 100
+ndim, nwalkers = len(p0), 32
 pos = [p0 + 1e-10 * np.random.randn(ndim) for i in xrange(nwalkers)]
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=16)
-sampler.run_mcmc(pos, 2)
+for i, (p, lp, state) in enumerate(sampler.sample(pos, iterations=1000)):
+    print(i)
 
 # Save the results.
 pickle.dump(sampler.chain, open("results.pkl", "wb"), -1)
